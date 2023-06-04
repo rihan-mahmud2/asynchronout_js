5eea4c55daf85e02d926eadbfae60be33686d02c
rihan <rihanmahmud1415@gmail.com>
2023-06-04 12:55:35 +0600

    then catch

--- index.js
+++ index.js
@@ -19,5 +19,44 @@ promise
     console.log(data);
   });
 
-Promise.resolve();
-Promise.reject();
+// promise implementation
+
+// function wait(ms) {
+//   const promise = new Promise((resolve) => {
+//     setTimeout(resolve, ms);
+//   });
+//   return promise;
+// }
+
+const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+wait(1000).then(() => {
+  console.log("Done in 1000ms");
+});
+wait(2000).then(() => {
+  console.log("Done in 2000ms");
+});
+wait(3000).then(() => {
+  console.log("Done in 3000ms");
+});
+
+const get = (url) => Promise.resolve();
+
+get(`users?userName=rihan`)
+  .then((user) => {
+    return get(`posts?userId=${user.id}`);
+  })
+  .then((posts) => {
+    const latestPost = posts[0];
+    return get(`comments?postId=${latestPost.id}`);
+  })
+  .then((comments) => {
+    const latestComment = comments[0];
+    return get(`user?commentId=${latestComment.id}`);
+  })
+  .then((user) => {
+    console.log(user);
+  })
+  .catch((e) => {
+    console.log("Error");
+  });
